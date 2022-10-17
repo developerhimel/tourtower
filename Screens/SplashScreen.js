a7ccc2fc71b17c12319b778538a7f18e0e673ca5
Himel Roy <freelancerhimel2814@gmail.com>
2022-10-17 09:14:31 +0600

    something

--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -9,29 +9,51 @@ import auth from '@react-native-firebase/auth';
 const SplashScreen = () => {
   const navigation = useNavigation();
   return (
-    <View>
-      <Text>SplashScreen</Text>
-      <Icon.Button name="facebook" backgroundColor="#3b5998">
-        Login with Facebook
-      </Icon.Button>
-      <Text>SplashScreen</Text>
-      <Animatable.View animation={'zoomIn'} duration={1000}>
-        <Button
-          mode="contained"
-          onPress={() => navigation.navigate('LoginScreen')}>
-          Login
-        </Button>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+      }}>
+      <Text style={styles.spacer}>SplashScreen</Text>
+      <Animatable.View animation={'bounceInDown'} duration={1000}>
+        <Icon.Button name="facebook" backgroundColor="#3b5998">
+          Login with Facebook
+        </Icon.Button>
       </Animatable.View>
-      <Text>SplashScreen</Text>
-      <Animatable.View animation={'zoomIn'} duration={1000}>
-        <Button mode="contained" onPress={() => auth().signOut()}>
-          Logout
-        </Button>
+
+      <Text style={styles.spacer}>SplashScreen</Text>
+      <Animatable.View animation={'bounceIn'} duration={1000}>
+        <Icon.Button name="google" backgroundColor="#a5a5a5">
+          Login with Google
+        </Icon.Button>
       </Animatable.View>
+
+      <Text style={styles.spacer}>SplashScreen</Text>
+      {auth().currentUser !== null ? (
+        <Animatable.View animation={'bounceInUp'} duration={1000}>
+          <Button mode="contained" onPress={() => auth().signOut()}>
+            Logout
+          </Button>
+        </Animatable.View>
+      ) : (
+        <Animatable.View animation={'bounceInUp'} duration={1000}>
+          <Button
+            mode="contained"
+            onPress={() => navigation.navigate('LoginScreen')}>
+            Login
+          </Button>
+        </Animatable.View>
+      )}
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  spacer: {
+    color: '#ffffff',
+  },
+});
